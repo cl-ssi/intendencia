1d0618e0a4d349b4d93313088f141b3faeaa6d36
Esteban <tebiccr@gmail.com>
2020-06-25 15:15:53 -0400

    redirige si la persona no tiene institucion

--- resources/views/help_basket/index.blade.php
+++ resources/views/help_basket/index.blade.php
@@ -4,6 +4,10 @@
 @section('content')
 @include('help_basket.nav')
 
+@if(!isset($institution->id))
+<script>window.location = "/home";</script>
+@endif
+
 <h3 class="mb-3">Listado de Canasta Entregadas {{$institution->name}}</h3>
 <div class="row">
     <div class="col-4 col-md-2">
